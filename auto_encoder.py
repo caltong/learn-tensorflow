fa0f36c2919ec9d72b8025ea1e92dce4b432c8aa
caltong <caltong1027@gmail.com>
2019-07-09 16:44:08 +0800

    auto_encoder.py

---
+++ auto_encoder.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sklearn.preprocessing as prep
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def xavier_init(fan_in, fan_out, constant=1):
+    low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
+    high = constant * np.sqrt(6.0 / (fan_in + fan_out))
+    return tf.random_uniform((fan_in, fan_out), minval=low, maxval=high, dtype=tf.float32)
+
+
+class AdditiveGaussianNoiseAutoEncoder(object):
+    def __init__(self, n_input, n_hidden, transfer_function=tf.nn.softplus, optimizer=tf.train.AdamOptimizer(),
+                 scale=0.1):
+        self.n_input = n_input
+        self.n_hidden = n_hidden
+        self.transfer = transfer_function
+        self.scale = tf.placeholder(tf.float32)
+        self.training_rate = scale
+        network_weights = self._initialize_weights()
+        self.weights = network_weights
