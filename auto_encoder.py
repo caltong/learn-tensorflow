96e41e1b16505f6108be53f811e7286a4319d8cd
caltong <caltong1027@gmail.com>
2019-07-12 17:13:01 +0800

    完善auto_encoder.py

--- auto_encoder.py
+++ auto_encoder.py
@@ -104,3 +104,17 @@ display_step = 1
 
 autoencoder = AdditiveGaussianNoiseAutoEncoder(n_input=784, n_hidden=200, transfer_function=tf.nn.softplus,
                                                optimizer=tf.train.AdamOptimizer(learning_rate=0.001), scale=0.01)
+
+# 开始训练过程
+for epoch in range(training_epochs):
+    avg_cost = 0.
+    total_batch = int(n_samples / batch_size)
+    for i in range(total_batch):
+        batch_xs = get_random_block_from_data(X_train, batch_size)
+        cost = autoencoder.partial_fit(batch_xs)
+        avg_cost = avg_cost + cost / n_samples * batch_size
+
+    if epoch % display_step == 0:
+        print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(avg_cost))
+
+print("Total cost:"+str(autoencoder.calc_total_cost(X_test)))
