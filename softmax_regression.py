fa083aee373b7c5b61aa74b3b02b6b6da49648f8
caltong <caltong1027@gmail.com>
2019-07-12 17:57:46 +0800

    实现softmax regression

--- softmax_regression.py
+++ softmax_regression.py
@@ -9,3 +9,26 @@ print(mnist.validation.images.shape, mnist.validation.labels.shape)  # images.sh
 
 sess = tf.InteractiveSession()  # 创建session
 x = tf.placeholder(tf.float32, [None, 784])  # 创建输入placeholder shape与mnist数据对应
+W = tf.Variable(tf.zeros([784, 10]))  # 初始化W为0
+b = tf.Variable(tf.zeros([10]))  # 初始化b为0
+
+# 实现softmax regression
+y = tf.nn.softmax(tf.add(tf.matmul(x, W)), b)
+
+# 定义交叉熵
+y_ = tf.placeholder(tf.float32, [None, 10])  # y' placeholder 定义
+cross_entropy = tf.reduce_mean(tf.reduce_sum(y_ * tf.log(y)))  # 交叉熵
+
+# 定义训练优化算法及超参数
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+tf.global_variables_initializer().run()  # 初始化全局变量
+
+# 迭代训练
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    train_step.run({x: batch_xs, y_: batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))  # 判断是否正确
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))  # 计算平均正确率
+print(accuracy.eval(({x: mnist.test.images, y_: mnist.test.labels})))  # 使用test数据评估正确率
