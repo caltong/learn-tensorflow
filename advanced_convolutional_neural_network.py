b33a65ea70a8bd26ce6a65e6cd8f0740774b2a3a
caltong <caltong1027@gmail.com>
2019-07-17 16:42:22 +0800

    cifar10 data

--- advanced_convolutional_neural_network.py
+++ advanced_convolutional_neural_network.py
@@ -1,3 +1,17 @@
 import tensorflow as tf
 import numpy as np
 import time
+
+max_steps = 3000
+batch_size = 128
+data_dir = 'cifar-10-batches-py/'
+
+
+# 读取数据
+def unpickle(file):
+    import pickle
+    with open(data_dir+file, 'rb') as fo:
+        data = pickle.load(fo, encoding='bytes')
+    return data
+
+
