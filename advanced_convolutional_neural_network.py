917e1d87413d6e9201918e97a52034aebc436221
caltong <caltong1027@gmail.com>
2019-07-18 20:13:50 +0800

    cifar10 CNN

--- advanced_convolutional_neural_network.py
+++ advanced_convolutional_neural_network.py
@@ -1,3 +1,12 @@
 import tensorflow as tf
 import numpy as np
 import time
+
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        data = pickle.load(fo, encoding='bytes')
+    return data
+
+
