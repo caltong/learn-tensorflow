104a8579045e8ebb735b9452c8c66b20892d4495
caltong <caltong1027@gmail.com>
2019-07-24 18:15:27 +0800

    cifar10 CNN

--- advanced_convolutional_neural_network.py
+++ advanced_convolutional_neural_network.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 import numpy as np
 import time
 
@@ -16,10 +17,20 @@ def unpickle(file):
 
 
 # 将数据存到list中
-cifar_data = []
-for i in range(5):
-    filename = 'data_batch_' + str(i + 1)
-    cifar_data.append(unpickle(filename))
+def load_train_data():
+    cifar_data = []
+    for i in range(5):
+        filename = 'data_batch_' + str(i + 1)
+        cifar_data.append(unpickle(filename))
+    return cifar_data
+
+
+# 读取test_data
+def load_test_data():
+    test_data = []
+    filename = 'test_batch'
+    test_data.append(unpickle(filename))
+    return test_data
 
 
 def variable_with_weight_loss(shape, stddev, wl):
@@ -29,3 +40,15 @@ def variable_with_weight_loss(shape, stddev, wl):
         tf.add_to_collection('losses', weight_loss)
     return var
 
+
+train_data = load_train_data()  # 读取训练数据
+images_train, labels_train = train_data[0][b'data'], train_data[0][b'labels']  # 图片信息与标签信息 暂时只使用batch_0
+test_data = load_test_data()  # 读取测试数据
+images_test, labels_test = test_data[0][b'data'], test_data[0][b'labels']  # 图片信息与标签信息
+
+images_train = images_train.reshape(10000, 32, 32, 3)
+images_test = images_test.reshape(10000, 32, 32, 3)
+print(images_train.shape)
+print(images_test.shape)
+
+plt.imshow(images_train[0])
