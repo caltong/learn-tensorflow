83eb589e287b270a2499e021c3458594078bfda2
caltong <caltong1027@gmail.com>
2019-08-01 15:49:51 +0800

    debug

--- alex_net.py
+++ alex_net.py
@@ -9,7 +9,7 @@ num_batches = 100
 
 # 显示网络每层结构函数
 def print_activations(t):
-    print(t.op.name, ' ', t.get_shape.as_list())
+    print(t.op.name, ' ', t.get_shape().as_list())
 
 
 # 结构
@@ -25,22 +25,22 @@ def inference(images):
         print_activations(conv1)  # 输出conv1结构
         parameters = parameters + [kernel, biases]  # 存入parameters list
 
-        lrn1 = tf.nn.lrn(conv1, 4, bias=1.0, alpha=0.001 / 9, beta=0.75, name='lrn1')
-        pool1 = tf.nn.max_pool(lrn1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool1')
-        print_activations(pool1)  # 输出pool1结构
+    lrn1 = tf.nn.lrn(conv1, 4, bias=1.0, alpha=0.001 / 9, beta=0.75, name='lrn1')
+    pool1 = tf.nn.max_pool(lrn1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool1')
+    print_activations(pool1)  # 输出pool1结构
 
     with tf.name_scope('conv2') as scope:
         kernel = tf.Variable(tf.truncated_normal([5, 5, 64, 192], dtype=tf.float32, stddev=1e-1), name='weights')
         conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding='SAME')
-        biases = tf.Variable(tf.constan(0.0, shape=[192], dtype=tf.float32), trainable=True, name='biases')
+        biases = tf.Variable(tf.constant(0.0, shape=[192], dtype=tf.float32), trainable=True, name='biases')
         bias = tf.nn.bias_add(conv, biases)
         conv2 = tf.nn.relu(bias, name=scope)
         print_activations(conv2)
         parameters = parameters + [kernel, biases]
 
-        lrn2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9, beta=0.75, name='lrn2')
-        pool2 = tf.nn.max_pool(lrn2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool2')
-        print_activations(pool2)
+    lrn2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9, beta=0.75, name='lrn2')
+    pool2 = tf.nn.max_pool(lrn2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name='pool2')
+    print_activations(pool2)
 
     with tf.name_scope('conv3') as scope:
         kernel = tf.Variable(tf.truncated_normal([3, 3, 192, 384], dtype=tf.float32, stddev=1e-1), name='weights')
@@ -54,9 +54,9 @@ def inference(images):
     with tf.name_scope('conv4') as scope:
         kernel = tf.Variable(tf.truncated_normal([3, 3, 384, 256], dtype=tf.float32, stddev=1e-1), name='weights')
         conv = tf.nn.conv2d(conv3, kernel, [1, 1, 1, 1], padding='SAME')
-        biases = tf.Variable(tf.truncated_normal(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
+        biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
         bias = tf.nn.bias_add(conv, biases)
-        conv4 = tf.nn.relu(bias, biases, name=scope)
+        conv4 = tf.nn.relu(bias, name=scope)
         parameters = parameters + [kernel, biases]
         print_activations(conv4)
 
@@ -87,7 +87,7 @@ def time_tensorflow_run(session, target, info_string):
         duration = time.time() - start_time
         if i >= num_steps_burn_in:  # 预热 排除显存加载 cache命中问题
             if not i % 10:
-                print('%s: step %d, duration = %.3f' % (datetime.now()), i - num_steps_burn_in, duration)
+                print('%s: step %d, duration = %.3f' % (datetime.now(), i - num_steps_burn_in, duration))
             total_duration = total_duration + duration
             total_duration_squared = total_duration_squared + duration * duration
 
@@ -114,5 +114,6 @@ def run_benchmark():
         objective = tf.nn.l2_loss(pool5)
         grad = tf.gradients(objective, parameters)
         time_tensorflow_run(sess, grad, "Forward-backward")
+
 
 run_benchmark()
