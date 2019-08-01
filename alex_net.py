a3493dac268dae304eaab89b519506c7f0bee57f
caltong <caltong1027@gmail.com>
2019-08-01 15:40:11 +0800

    完成AlexNet

--- alex_net.py
+++ alex_net.py
@@ -73,3 +73,46 @@ def inference(images):
     print_activations(pool5)
 
     return pool5, parameters
+
+
+# 评估运算时间
+def time_tensorflow_run(session, target, info_string):
+    num_steps_burn_in = 10
+    total_duration = 0.0
+    total_duration_squared = 0.0
+
+    for i in range(num_batches + num_steps_burn_in):
+        start_time = time.time()
+        _ = session.run(target)
+        duration = time.time() - start_time
+        if i >= num_steps_burn_in:  # 预热 排除显存加载 cache命中问题
+            if not i % 10:
+                print('%s: step %d, duration = %.3f' % (datetime.now()), i - num_steps_burn_in, duration)
+            total_duration = total_duration + duration
+            total_duration_squared = total_duration_squared + duration * duration
+
+    mn = total_duration / num_batches
+    vr = total_duration_squared / num_batches - mn * mn
+    sd = math.sqrt(vr)
+
+    print('%s:%s across %d steps, %.3f +/- %.3f sec / batch' % (datetime.now(), info_string, num_batches, mn, sd))
+
+
+def run_benchmark():
+    with tf.Graph().as_default():
+        image_size = 224
+        images = tf.Variable(tf.random_normal([batch_size, image_size, image_size, 3], dtype=tf.float32, stddev=1e-1))
+
+        pool5, parameters = inference(images)
+
+        init = tf.global_variables_initializer()
+        sess = tf.Session()
+        sess.run(init)
+
+        time_tensorflow_run(sess, pool5, "Forward")
+
+        objective = tf.nn.l2_loss(pool5)
+        grad = tf.gradients(objective, parameters)
+        time_tensorflow_run(sess, grad, "Forward-backward")
+
+run_benchmark()
